98e0d0012c2851b27b313debdc6a4dabf8481e7d
SylviaRemington <sylviaremington@gmail.com>
2025-07-06 12:44:22 -0700

    stopping point for code-along

--- models/user.js
+++ models/user.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
+const applicationSchema = new mongoose.Schema ({
+  //properties of applications
+  // ! stopping here with code-along and just going to watch so get through it - stopped at Skyrockit lecture 33:13 minutes in
+  // ! Later, I'll come back to this and code-along for entire lesson after Project 2 is done
+})
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -10,6 +16,9 @@ const userSchema = mongoose.Schema({
     required: true,
   },
 });
+
+//This is the application data on the user
+applications: [applicationSchema], // embed applicationSchema here
 
 const User = mongoose.model('User', userSchema);
 
